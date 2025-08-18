556158a0c50c4f8481a260b4b1a525957c04f09a
daorsforgealsystems <bakir@daorsforgealsystems.com>
2025-08-18 03:50:03 +0200

    Update AppProviders.tsx

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from '@/context/AuthContext';
 import { LayoutProvider } from '@/context/LayoutContext';
 import { NavigationProvider } from '@/context/NavigationContext';
+import { AnimationProvider } from './AnimationProvider';
 import ErrorBoundary from '@/components/ErrorBoundary';
 
 interface AppProvidersProps {
@@ -12,15 +13,17 @@ interface AppProvidersProps {
 export const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
   return (
     <ErrorBoundary>
-      <Router>
-        <AuthProvider>
-          <LayoutProvider>
-            <NavigationProvider>
-              {children}
-            </NavigationProvider>
-          </LayoutProvider>
-        </AuthProvider>
-      </Router>
+      <AnimationProvider>
+        <Router>
+          <AuthProvider>
+            <LayoutProvider>
+              <NavigationProvider>
+                {children}
+              </NavigationProvider>
+            </LayoutProvider>
+          </AuthProvider>
+        </Router>
+      </AnimationProvider>
     </ErrorBoundary>
   );
 };
